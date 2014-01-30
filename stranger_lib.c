94bfd077b6f3342de909948edd9cde3f24ff2c14
malkhalaf <muath.alkhalaf@gmail.com>
2014-01-30 14:10:07 -0800

    Update stranger_lib.c

--- stranger_lib.c
+++ stranger_lib.c
@@ -1,12 +1,6 @@
-/* stranger.c
- *  Author: Fang Yu   June 2007
- *  Stranger is the back end for a joint project with Marco in CS272, Spr07.
- *  Stranger uses MONA DFA package to perform string analysis.
- */
-
 /*
- * MONA
- * Copyright (C) 1997-2007 BRICS.
+ * Stranger
+ * Copyright (C) 2013-2014 University of California Santa Barbara.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,10 +14,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the  Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ * Foundation, Inc., 51 Franklin Street, Suite 500, Boston, MA 02110-1335,
  * USA.
+ *
+ * Authors: Fang Yu, Muath Alkhalaf
  */
-
 
 #include "stranger.h"
 #include <stdlib.h>
